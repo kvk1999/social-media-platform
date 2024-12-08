32b01cda5278342fbf5a6e10ddc5ee7106ab5c1f
kvk1999 <koushikraghavan@gmail.com>
2024-12-08 22:07:16 +0530

    updated

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
+const Post = require('../models/Post'); // Ensure case matches the file name
 const User = require('../models/User');
 const Comment = require('../models/Comment');
 
