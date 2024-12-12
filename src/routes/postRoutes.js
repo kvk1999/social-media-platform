b03650c47bb21c2a3ee4e521e2458fbdf2832fc3
kvk1999 <koushikraghavan@gmail.com>
2024-12-12 13:17:25 +0530

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPost, getPosts, getPost, updatePost, deletePost } from '../controllers/postController.js';  // Correct import
+import { createPost, getPosts, getPost, updatePost, deletePost, likePost, unlikePost } from '../controllers/postController.js';  // Correct import
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.get('/', getPosts);
 router.get('/:postId', getPost);  // This will call the getPost function
 router.put('/:postId', updatePost);
 router.delete('/:postId', deletePost);
+router.patch('/:postId/like', likePost);  // Like a post
+router.patch('/:postId/unlike', unlikePost);  // Unlike a post
 
 export default router;
