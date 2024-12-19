d25318049689219857b4ab861984f1b1e9534b29
kvk1999 <koushikraghavan@gmail.com>
2024-12-19 18:20:33 +0530

    update

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,7 +1,16 @@
 import multer from "multer";
 
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage(); // Store file in memory for further processing
 
-const uploadFile = multer({ storage }).single("file");
+const uploadFile = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/") || file.mimetype.startsWith("video/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type. Only images and videos are allowed."));
+    }
+  },
+}).single("file");
 
 export default uploadFile;
